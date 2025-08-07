90b8d7fc0fce5262257dc4eac1acb932fea3ed79
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-07 14:28:55 -0400

    rendered blog posts

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,4 +1,11 @@
+import { posts } from './lib/posts'
+import {useState} from 'react'
+
 const  BlogList = () => {
+
+    const [blogPosts, setBlogPosts] = useState(posts)
+
+    console.log(blogPosts)
     return (
         <div>
             <h1>Blog  Posts</h1>
