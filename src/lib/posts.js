98d36517331efe1d55dbd59cbcabd27043aff6ff
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-07 14:05:10 -0400

    initial commit

---
+++ src/lib/posts.js
@@ -0,0 +1,20 @@
+export const posts = [
+  {
+    id: 1,
+    slug: "my-first-post",
+    title: "My First Post",
+    content: "This is the content of my first post."
+  },
+  {
+    id: 2,
+    slug: "learning-react-router",
+    title: "Learning React Router",
+    content: "React Router makes navigation in React apps easy and powerful."
+  },
+  {
+    id: 3,
+    slug: "dynamic-routing-rocks",
+    title: "Dynamic Routing Rocks!",
+    content: "Explore how to use dynamic routing with URL parameters."
+  }
+];
